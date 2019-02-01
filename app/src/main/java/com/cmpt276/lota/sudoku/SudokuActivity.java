3b46887700848719fd76c4fa8fc361f3743f7121
Jerrick CAI <caijiali2000@gmail.com>
2019-01-31 23:33:22 -0800

    test draw & view

--- app/src/main/java/com/cmpt276/lota/sudoku/SudokuActivity.java
+++ app/src/main/java/com/cmpt276/lota/sudoku/SudokuActivity.java
@@ -4,20 +4,35 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SudokuActivity extends AppCompatActivity {
 
     MyView customView;
-
+    View gameView = (View) findViewById(R.id.game_view);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_sudoku);
+
         customView= new MyView(this.getBaseContext());
-        this.setContentView(customView);
+        //this.setContentView(customView);
 
         //customView = findViewById(R.id.my_view);
+
+
+        gameView.addView(customView);
+        setContentView(R.layout.activity_sudoku);
+
+//        TextView valueTV = new TextView(this);
+//        valueTV.setText("hallo hallo");
+//        valueTV.setId(5);
+//        valueTV.setLayoutParams(new LayoutParams(
+//                LayoutParams.FILL_PARENT,
+//                LayoutParams.WRAP_CONTENT));
+//
+//        ((LinearLayout) linearLayout).addView(valueTV);
 
 
     }
