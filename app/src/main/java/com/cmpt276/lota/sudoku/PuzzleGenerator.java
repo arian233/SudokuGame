b540dcc749f94a40d5fb07ccd2d14e4716c945fb
Luming Zhang <lza105@sfu.ca>
2019-03-01 10:33:06 -0800

    Make the puzzle random
    make the puzzle random

--- app/src/main/java/com/cmpt276/lota/sudoku/PuzzleGenerator.java
+++ app/src/main/java/com/cmpt276/lota/sudoku/PuzzleGenerator.java
@@ -1,37 +1,61 @@
 package com.cmpt276.lota.sudoku;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PuzzleGenerator {
     private static String lan1[];// to record two languages, may needs to change it, if the later iteration requires more than two languages.
     private static String lan2[];
     private static final int lanSize = 10;
     private static final int puzzleSize = 9;
+    private ArrayList<ArrayList<Integer>> Available = new ArrayList<ArrayList<Integer>>();
+    private Random rand = new Random();
+    public static boolean conflict = true; //True if there is a conflict
 
     /**
      * Constructor: to have a 2d array of preset two languages
      */
     PuzzleGenerator(){
-        //
         lan1 = new String[] {"dummy","one", "two","three","four", "five","six","seven", "eight","nine"};
         lan2 = new String[] {"dummy","一", "二","三","四", "五","六","七", "八","九"};
+
     }
 
+
     /**
-     * to generate puzzle
-     * @return a generated puzzle
+     * Constructor: to have a 2d array of preset two languages
      */
-    public static Language[][] generatePuzzle(){
-        int tmp[][] = new int[][]{
-                { 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 4, 5, 6, 0, 8, 0, 1, 0, 0 },
-                { 7, 8, 9, 1, 0, 3, 0, 0, 6 },
-                { 0, 1, 0, 3, 6, 0, 0, 9, 7 },
-                { 3, 6, 5, 8, 9, 7, 2, 1, 0 },
-                { 8, 0, 7, 2, 1, 4, 3, 6, 5 },
-                { 5, 0, 1, 0, 4, 2, 9, 0, 8 },
-                { 6, 4, 2, 9, 0, 8, 5, 3, 1 },
-                { 0, 7, 8, 5, 0, 1, 0, 4, 0 } };
+    public Language[][] generateGrid(){
+        int[][] tmp = new int[puzzleSize][puzzleSize];
+        int currentPos = 0;
+        clearGrid(tmp);
+
+        while (currentPos < 81){
+            if (Available.get(currentPos).size() != 0){ //if arraylist is not empty
+                int i = rand.nextInt(Available.get(currentPos).size()); // get random number
+                int number = Available.get(currentPos).get(i);
+
+                if (!checkConflict(tmp, currentPos, number)){
+                    conflict = false;
+                    int xPos = currentPos % puzzleSize;
+                    int yPos = currentPos / puzzleSize;
+                    tmp[xPos][yPos] = number;
+                    Available.get(currentPos).remove(i);
+
+                    currentPos++;
+                }
+                else{
+                    Available.get(currentPos).remove(i);
+                }
+            }else{
+                for (int i = 1; i <= puzzleSize; i++){
+                    Available.get(currentPos).add(i);
+                }
+                currentPos--;
+            }
+        }
+
+        removeElments(tmp);
 
         Language puzzle[][]=new Language[puzzleSize][puzzleSize];
         for(int j=0;j<puzzleSize;j++) {
@@ -42,8 +66,112 @@ public class PuzzleGenerator {
                     puzzle[j][i]=new Language(tmp[j][i], lan1[tmp[j][i]], lan2[tmp[j][i]],0);//0 means dummy
             }
         }
+
+
         return puzzle;
     }
+
+    private void removeElments(int[][] puzzle)
+    {
+        int removeNumber = 0;
+        while(removeNumber < 20)
+        {
+            int x_location = rand.nextInt(9);
+            int y_location = rand.nextInt(9);
+            if (puzzle[x_location][y_location] != 0)
+            {
+                puzzle[x_location][y_location] = 0;
+                removeNumber++;
+            }
+        }
+        conflict = getConflict(puzzle);
+    }
+
+    private void clearGrid(int[][] puzzle){
+        Available.clear();                  //Clears the arrayList
+
+        for (int y = 0; y < 9; y++){
+            for (int x = 0; x < 9; x++){
+                puzzle[x][y] = -1;
+            }
+        }
+
+        for (int x = 0; x < 81; x++){       // add 1 to 9 to arraylist
+            Available.add(new ArrayList<Integer>());
+            for (int i = 1; i <= 9; i++){
+                Available.get(x).add(i);
+            }
+        }
+    }
+
+    public boolean getConflict(int[][] puzzle){
+        for (int currentPos = 0; currentPos < 81; currentPos++) {
+            int xPos = currentPos % 9;
+            int yPos = currentPos / 9;
+            if (puzzle[xPos][yPos] != -1){
+                if (checkConflict(puzzle, currentPos, puzzle[xPos][yPos])){
+                    return true;
+                }
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkConflict(int[][] puzzle, int currentPos, final int number){
+        int xPos = currentPos % 9;
+        int yPos = currentPos / 9;
+
+        if (checkHorizontalConflict(puzzle, xPos, yPos, number) || checkVerticalConflict(puzzle, xPos, yPos, number)
+                || checkRegionConflict(puzzle, xPos, yPos, number)){
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Return true if there is a conflict
+     * @param puzzle
+     * @param xPos
+     * @param yPos
+     * @param number
+     * @return
+     */
+    private boolean checkHorizontalConflict(final int[][] puzzle, final int xPos, final int yPos, final int number){
+        for (int x = xPos -1; x >= 0; x--){
+            if (number == puzzle[x][yPos]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkVerticalConflict(final int[][] Sudoku, final int xPos, final int yPos, final int number){
+        for (int y = yPos - 1; y >= 0; y--){
+            if (number == Sudoku[xPos][y]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkRegionConflict(final int[][] Sudoku, final int xPos, final int yPos, final int number){
+        int xRegion = xPos / 3;
+        int yRegion = yPos / 3;
+
+        for (int x = xRegion * 3; x < xRegion * 3 + 3; x++){
+            for (int y = yRegion * 3; y < yRegion * 3 + 3; y++){
+                if ((x != xPos || y != yPos) && number == Sudoku[x][y]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     /**
      * to return LanguageOne array
